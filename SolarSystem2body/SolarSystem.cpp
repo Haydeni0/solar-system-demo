86b69291e3a6a6322aa7eaba03c6c09764e0f169
Haydeni0 <hayden.dorahy@gmail.com>
2021-04-21 21:39:28 +0100

    Initial commit to set up environment

---
+++ SolarSystem2body/SolarSystem.cpp
@@ -0,0 +1,33 @@
+
+
+/* 
+* First create a solar system where the star is in a fixed position and the planets onlly interact with the sun not each other
+* This will give analytic results for the equations of motion of the planets
+* Use astronomical units (AU) for distances, solar masses for the mass of stars
+* Use class inheritance for individual stars and planets based off a base star and a base planet class
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
